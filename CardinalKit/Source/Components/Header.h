e0ca8af0ff62ca69bc095dca0dc8eefb4f32ffe6
EstebanRamosFractal <julianramos@alternova.co>
2022-10-27 18:45:04 -0500

    remove granola import

--- CardinalKit/Source/Components/Header.h
+++ CardinalKit/Source/Components/Header.h
@@ -5,9 +5,9 @@
 //  Created by Julian Esteban Ramos Martinez on 8/04/21.
 //
 
-#ifndef Header_h
-#define Header_h
-
-#import "Granola/OMHSerializer.h"
-
-#endif /* Header_h */
+//#ifndef Header_h
+//#define Header_h
+//
+//#import "Granola/OMHSerializer.h"
+//
+//#endif /* Header_h */
